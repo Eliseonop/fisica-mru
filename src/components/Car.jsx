662f91e5e06339775ab736f50e598726f7f862ab
eliseodev <eliseonop@gmail.com>
2023-11-28 14:31:18 -0500

    update: changes log

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
+const SCALE = 100 // 100 píxeles = 1 metro
 
-const Car = ({ car }) => {
+const Car = ({ car, distanceToNextCar }) => {
   const carStyle = {
     width: '150px',
     height: '150px',
@@ -8,39 +9,50 @@ const Car = ({ car }) => {
     left: `${car.position.x}px`,
     top: `${car.position.y}px`
   }
+  const distanceToNextCarInMeters = distanceToNextCar / SCALE
   return (
     <div style={carStyle} className='relative'>
-      <div
-        className='text-sm bg-slate-400 rounded-md absolute -top-28 p-1 left-0
-        '
-      >
-        {/* informacion before como un triangulo inferior */}
+      <div className='text-sm bg-slate-400 rounded-md absolute -top-28 p-1 left-0'>
+        {/* Información del carro y triángulo inferior */}
         <div
           className='flex flex-col h-full text-sm w-auto
-        
-          before:content-[""] before:w-5 before:h-4 before:bg-slate-400 before:rounded-full before:absolute before:-bottom-2 before:left-1/2 
-          before:transform before:-translate-x-1/2 before:rotate-45 
-          before:-z-10
-        '
+            before:content-[""] before:w-5 before:h-4 before:bg-slate-400 before:rounded-full before:absolute before:-bottom-2 before:left-1/2 
+            before:transform before:-translate-x-1/2 before:rotate-45 
+            before:-z-10'
         >
+          <div className='text-white font-bold text-sm'>
+            <span className='inline-block rounded-full mr-1 text-black font-semibold'>
+              Distancia al siguiente carro:
+            </span>
+            {distanceToNextCarInMeters.toFixed(2)} m
+          </div>
           <div className='text-white text-sm font-bold'>
-            <span className='inline-block  rounded-full mr-1 text-black font-semibold'>
+            <span className='inline-block rounded-full mr-1 text-black font-semibold'>
               Nombre
             </span>
             {car.name}
           </div>
           <div className='text-white font-bold text-sm'>
-            <span className='inline-block  rounded-full mr-1 text-black font-semibold'>
+            <span className='inline-block rounded-full mr-1 text-black font-semibold'>
               Peso
             </span>
             {car.weight} kg
           </div>
           <div className='text-white font-bold text-sm'>
-            <span className='inline-block  rounded-full mr-1 text-black font-semibold'>
+            <span className='inline-block rounded-full mr-1 text-black font-semibold'>
               Velocidad
             </span>
             {car.velocity} m/s
           </div>
+          {/* Mostrar la distancia al siguiente carro */}
+          {/* {distanceToNextCar !== undefined && (
+            <div className='text-white font-bold text-sm'>
+              <span className='inline-block rounded-full mr-1 text-black font-semibold'>
+                Distancia al siguiente:
+              </span>
+              {distanceToNextCar.toFixed(2)} px
+            </div>
+          )} */}
         </div>
       </div>
       <img
@@ -63,7 +75,8 @@ Car.propTypes = {
       x: PropTypes.number.isRequired,
       y: PropTypes.number.isRequired
     }).isRequired
-  }).isRequired
+  }).isRequired,
+  distanceToNextCar: PropTypes.number // Esta prop puede ser undefined al inicio, por lo tanto no es requerida
 }
 
 export default Car
--- src/components/CarField.jsx
+++ src/components/CarField.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Car from './Car'
 
@@ -9,9 +9,14 @@ const CarField = ({
   setIsMoving,
   frictionCoefficient
 }) => {
+  const [distances, setDistances] = useState([])
+
+  const calculateDistanceBetweenCars = (car1, car2) => {
+    return Math.abs(car1.position.x - car2.position.x)
+  }
+
   const calculateBrakingDistance = (velocity, weight) => {
     const gravity = 9.8
-    // Convertir la velocidad a m/s desde km/h y aplicar la fórmula
     const velocityInMetersPerSecond = (velocity * 1000) / 3600
     return (
       (Math.pow(velocityInMetersPerSecond, 2) * weight) /
@@ -20,62 +25,59 @@ const CarField = ({
   }
 
   const updatePosition = (prevPosition, velocity, weight) => {
-    // Convertir la velocidad a m/s y luego a 'píxeles' asumiendo 100px = 100m
     const velocityInMetersPerSecond = (velocity * 1000) / 3600
     let newPositionX =
       prevPosition.x + (velocityInMetersPerSecond / weight) * 100
     if (newPositionX > window.innerWidth) {
-      newPositionX -= window.innerWidth // Ajuste para reaparecer en el otro lado
+      newPositionX -= window.innerWidth
     }
     return { ...prevPosition, x: newPositionX }
   }
+
   useEffect(() => {
     let interval
     if (isMoving) {
       interval = setInterval(() => {
+        let newDistances = []
         cars.forEach((car, i) => {
+          const nextCar = cars[(i + 1) % cars.length]
+          const distanceToNextCar = calculateDistanceBetweenCars(car, nextCar)
+
+          newDistances.push(distanceToNextCar)
+
           const newPosition = updatePosition(
             car.position,
             car.velocity,
             car.weight
           )
 
-          const nextCarPosition = cars[(i + 1) % cars.length].position.x
+          const brakingDistance = calculateBrakingDistance(
+            car.velocity,
+            car.weight
+          )
 
-          // Comprobar la necesidad de frenar
-          if (
-            Math.abs(newPosition.x - nextCarPosition) <
-            calculateBrakingDistance(car.velocity)
-          ) {
-            setIsMoving(false)
-          } else if (Math.abs(newPosition.x - nextCarPosition) < 50) {
-            // Asumiendo que el ancho del carro es de 50px
-            // Detectar colisión
-            setIsMoving(false)
-          } else {
-            setCarPosition(car.id, newPosition)
-          }
+          // if (
+          //   distanceToNextCar < brakingDistance ||
+          //   distanceToNextCar < 50 // asumiendo un ancho de carro de 50px
+          // ) {
+          //   setIsMoving(false)
+          // } else {
+          setCarPosition(car.id, newPosition)
+          // }
         })
+        setDistances(newDistances)
       }, 50)
     }
 
     return () => clearInterval(interval)
-  }, [
-    isMoving,
-    cars,
-    setCarPosition,
-    setIsMoving,
-    frictionCoefficient,
-    calculateBrakingDistance
-  ])
+  }, [isMoving, cars, setCarPosition, setIsMoving, frictionCoefficient])
 
   return (
     <>
       <div className='flex z-10 flex-col relative w-full h-1/2 items-center justify-center'>
         {cars.map((car, i) => (
-          <Car key={i} car={car} />
+          <Car key={i} car={car} distanceToNextCar={distances[i]} />
         ))}
-        {/* un div con el color de una pista */}
       </div>
       <div className='absolute w-full h-48 top-28 bg-gray-500'></div>
     </>
